b8dae443948bbbff45ae00fe3fc1e03ab0d2c183
galphonsemaynard@gmail.com <galphonsemaynard@gmail.com>
2020-12-03 15:53:58 -0500

    Includes assumptions that the model is based on and source material (Section 2)

--- IBM_header.R
+++ IBM_header.R
@@ -59,6 +59,78 @@ gheritability=0.2
 nDams=0
 ## Number of simulations to run
 simNum=1
+## Number of years to simulate
+nYears=100
+## Length of burn-in (when the system will run without dams for a time
+##    in order to stabilize). Time is in years. 
+maxBurn=20
+## Parameterize the river system length (km) and wetted habitat area
+##    (m2). These values were taken from the description of the 
+##    Narraguagus River in the 2015 Annual Report of the U.S. Atlantic
+##    Salmon Committee, which can be found at
+##    http://www.nefsc.noaa.gov/USASAC/Reports/USASAC2015-Report%2327-2014-Activities.pdf
+rkm=89
+W=502763
 ## -----------------------------------
 ## SECTION 2: ASSUMPTIONS
-##
+## Set the advantage for >=2SW males in the mating system. For example
+##    if mating is "2", then the >=2SW males are twice as likely to be
+##    drawn from the pool as grilse. If mating is "3", they are three
+##    times as likely, etc. This values is not limited to whole numbers
+mating=1.92
+## Parameterize the Ricker Curve, values based on:
+## Prevost, E. 2003. Setting biological reference points for Atlantic
+##    Salmon stocks: transfer of information from data-rich to
+##    sparse-data situation by Bayesian hierarchical modelling. ICES
+##    Journal of Marine Science. 60(6):1177-1193.
+##    DOI: 10.1016/j.icesjms.2003.08.001
+A=0.420250111
+B=0.0000005
+## Set the starting distributions of fish sizes to either "modern" or
+##    "historic". "modern" sizes are based on fish measured by the 
+##    Maine Dept. of Marine Resources at the Veazie Dam fish trap on
+##    the Penobscot River between 1979-2015. "historic" sizes are lengths
+##    that have been back-calculated from weights measured by U.S.
+##    Commission on Fish and Fisheries Agents at the Bucksport, Maine
+##    fish market at the mouth of the Penobscot River in 1879. Lengths
+##    were back-calculated using the average of three Atlantic Salmon
+##    length/weight curves (DFO Canada, Icelandic Institute for 
+##    Freshwater Science, and the River Tweed Commission of Scotland). 
+startSize="historic"
+## Set survival values
+## Hatching to smolt survival is taken from 
+## Legault, Christopher M. 2005. Population viability analysis of 
+##    Atlantic Salmon in Maine, USA. Transactions of the American 
+##    Fisheries Society. 134(3):549-562. 
+##    DOI: 10.1577/T04-017.1
+freshwaterSurvival=0.041426
+## Per river kilometer survival of smolts at dams is based off the 
+##    target value for downstream survival at hydroelectric dams in
+##    the Penobscot River system
+damSurvive=0.95
+## Annual survival at sea is taken from
+## Chaput, G. 2012. Overview of the status of Atlantic Salmon (Salmo
+##    salar) in the North Atlantic and trends in marine mortality. ICES
+##    Journal of Marine Science. 69(9):1538-1548
+##    DOI: 10.1093/icesjms/fss013
+marineSurvival=0.24
+## Outmigration survival for kelts is based on the number of dams
+##    present and is taken from 
+## Maynard, George A., Lisa K. Izzo, and Joseph D. Zydlewski. 2018. 
+##    Movement and mortality of Atlantic Salmon kelts (Salmo salar)
+##    released into the Penobscot River, Maine. Fishery Bulletin. 
+##    116(3-4):281-290. DOI: 10.755/FB.116.3-4.6
+if(nDams==0){
+  keltSurvival=0.63
+} else {
+  keltSurvival=0.429^nDams
+}
+## At sea growth for adults is assumed to be between 0 and 5 cm annually
+## based on
+## Izzo, Lisa K. and Joseph D. Zydlewski. 2018. Retrospective analysis
+##    of seasonal ocean growth rates of two sea winter Atlantic Salmon 
+##    in Eastern Maine using historic scales. Marine and Coastal Fisheries.
+##    9(1):357-372. 
+##    DOI: 10.1080/19425120.2017.1334723
+maxGrowth=5
+## -----------------------------------
