a038fe90a085d34a2d40ea9b0b374114c2939e74
george <galphonsemaynard@gmail.com>
2021-12-24 06:50:21 -0500

    keep heritability static at 0.2

--- IBM_SimRunner.sh
+++ IBM_SimRunner.sh
@@ -2,10 +2,12 @@
 
 #!/bin/bash
 ## Set heritability values for size and growth
-for sH in 0.1 0.2 0.3 0.4 0.5
+## for sH in 0.1 0.2 0.3 0.4 0.5
+for sH in 0.2
 	do
 	s=${sH: -1}
-	for gH in 0.1 0.2 0.3 0.4 0.5
+##	for gH in 0.1 0.2 0.3 0.4 0.5
+  for gH in 0.2
 		do
 		g=${gH: -1}
 		for nD in {0..5}
